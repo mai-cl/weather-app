c199e951c82eaab4f154b41e298e8e42fcffa8bb
Rodrigo Canaviri <rodrigomt.cl@gmail.com>
2022-09-28 16:41:09 -0300

    Fix navlink decoration

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,74 +1,45 @@
 import { Box, Container, Tab, Tabs } from '@mui/material'
-import PropTypes from 'prop-types'
-import React, { useState } from 'react'
-import { Link as RouterLink } from 'react-router-dom'
+import { Link as RouterLink, matchPath, useLocation } from 'react-router-dom'
+
+function useRouteMatch(patterns) {
+  const { pathname } = useLocation()
+
+  for (let i = 0; i < patterns.length; i += 1) {
+    const pattern = patterns[i]
+    const possibleMatch = matchPath(pattern, pathname)
+    if (possibleMatch !== null) {
+      return possibleMatch
+    }
+  }
+
+  return null
+}
 
 const Navbar = () => {
-  const [value, setValue] = useState(0)
-
-  const handleChange = (event, newValue) => {
-    setValue(newValue)
-  }
+  const routeMatch = useRouteMatch(['/today', '/hourly', '/days'])
+  const currentTab = routeMatch?.pattern?.path
 
   return (
     <Container component='nav' sx={{ marginBottom: '20px', marginTop: '20px' }}>
       <Box sx={{ borderBottom: 1, borderColor: 'divider' }}>
-        <Tabs
-          value={value}
-          onChange={handleChange}
-          aria-label='basic tabs example'
-        >
+        <Tabs value={currentTab} aria-label='basic tabs example'>
           <Tab
             label='Today'
-            {...a11yProps(0)}
+            value='/today'
             to='/today'
             component={RouterLink}
           />
           <Tab
             label='Hourly'
-            {...a11yProps(1)}
+            value='/hourly'
             to='/hourly'
             component={RouterLink}
           />
-          <Tab
-            label='3 Day'
-            {...a11yProps(2)}
-            to='/days'
-            component={RouterLink}
-          />
+          <Tab label='3 Days' value='/days' to='/days' component={RouterLink} />
         </Tabs>
       </Box>
     </Container>
   )
-}
-
-function TabPanel(props) {
-  const { children, value, index, ...other } = props
-
-  return (
-    <div
-      role='tabpanel'
-      hidden={value !== index}
-      id={`simple-tabpanel-${index}`}
-      aria-labelledby={`simple-tab-${index}`}
-      {...other}
-    >
-      {value === index && <Box sx={{ p: 3 }}>{children}</Box>}
-    </div>
-  )
-}
-
-TabPanel.propTypes = {
-  children: PropTypes.node,
-  index: PropTypes.number.isRequired,
-  value: PropTypes.number.isRequired,
-}
-
-function a11yProps(index) {
-  return {
-    id: `simple-tab-${index}`,
-    'aria-controls': `simple-tabpanel-${index}`,
-  }
 }
 
 export default Navbar
